9f7837139e6deb0ca78816d98142501c19a905bd
RJKannadaAdmin <83166159+RJKannadaAdmin@users.noreply.github.com>
2021-04-26 11:23:19 +0530

    Update main_assistant.py

--- assistant/main_assistant.py
+++ assistant/main_assistant.py
@@ -39,7 +39,7 @@ async def start(client, message):
     bot_username = starkbot.username
     firstname = message.from_user.first_name
     user_id = message.from_user.id
-    starttext = f"`Hello, {firstname} ! Nice To Meet You, Well I Am {bot_name}, An Powerfull Assistant Bot To Talk And Do Many Things For My Master!`. \n\nPowered By [Friday Userbot](t.me/FridayOT)"
+    starttext = f"`Hello, {firstname} ! Nice To Meet You, Well I Am {bot_name}, An Powerfull Assistant Bot To Talk And Do Many Things For My Master!`. \n\nDeveloped By [The Hacker ](t.me/RJ_KannadaAdmin)"
     mypic = Config.ASSISTANT_START_PIC
     if user_id not in all_user_s:
         await client.send_photo(
