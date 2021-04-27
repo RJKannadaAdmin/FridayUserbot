89969a3976edaaa66d62bb123cb5bd13335b9247
RJKannadaAdmin <83166159+RJKannadaAdmin@users.noreply.github.com>
2021-04-27 12:56:06 +0530

    Update config_var.py

--- main_startup/config_var.py
+++ main_startup/config_var.py
@@ -43,7 +43,7 @@ class Config(object):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     STRINGSESSION = os.environ.get("STRINGSESSION", None)
     ASSISTANT_START_PIC = os.environ.get(
-        "ASSISTANT_START_PIC", "https://telegra.ph/file/8dbe8690a6071a910bd55.jpg"
+        "ASSISTANT_START_PIC", "https://telegra.ph/file/ec306db1f0d58c3ae0083.jpg"
     )
     STRINGSESSION_2 = os.environ.get("STRINGSESSION_2", None)
     STRINGSESSION_3 = os.environ.get("STRINGSESSION_3", None)
@@ -65,7 +65,7 @@ class Config(object):
         "UPSTREAM_REPO", "https://github.com/RJKannadaAdmin/"
     )
     ALIVE_IMG = os.environ.get(
-        "ALIVE_IMG", "https://telegra.ph/file/8dbe8690a6071a910bd55.jpg"
+        "ALIVE_IMG", "https://telegra.ph/file/ce64e061cd322de98207c.jpg"
     )
     U_BRANCH = "master"
     HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
